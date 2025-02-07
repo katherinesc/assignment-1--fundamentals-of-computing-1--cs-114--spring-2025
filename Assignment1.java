71831b766b0ee01212d9815605cd272a3c032e70
katherinesc <kcorrea10506@gmail.com>
2025-02-06 21:42:24 -0500

    Created the Letter C

--- Assignment1.java
+++ Assignment1.java
@@ -3,8 +3,9 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
-    System.out.println("KKK\tKKK\n" + "KKK\t\bKKK\n" +
-    "KKK\t\b\bKKK\n" + "KKK\t\b\b\b\bKKKK\n" +"KKK\t\b\b\bKKK\n" +
-    "KKK\t\b\bKKK\n" + "KKK\tKKK\n" );
+    System.out.println("KKK\tKKK\tCCCCCCCCCCC\n" +
+    "KKK\t\bKKK\tCCCCCCCCCCC\n" + "KKK\t\b\bKKK\tCCC\n" +
+    "KKK\t\b\b\b\bKKKK\tCCC\n" + "KKK\t\b\b\bKKK\tCCC\n" +
+    "KKK\t\b\bKKK\tCCCCCCCCCCC\n" + "KKK\tKKK\tCCCCCCCCCCC\n" );
   }
 }
