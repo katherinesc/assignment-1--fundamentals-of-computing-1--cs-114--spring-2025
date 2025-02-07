794f35c99419908e10cdb703d07936667ecca12b
katherinesc <kcorrea10506@gmail.com>
2025-02-06 21:34:03 -0500

    Created the Letter K

--- Assignment1.java
+++ Assignment1.java
@@ -3,5 +3,8 @@
 public class Assignment1 {
   public static void main(String[] args) {
     // Add your code here
+    System.out.println("KKK\tKKK\n" + "KKK\t\bKKK\n" +
+    "KKK\t\b\bKKK\n" + "KKK\t\b\b\b\bKKKK\n" +"KKK\t\b\b\bKKK\n" +
+    "KKK\t\b\bKKK\n" + "KKK\tKKK\n" );
   }
 }
